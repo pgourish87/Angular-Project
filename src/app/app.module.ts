419bfc02bb6ffd5d9506e0ede0e3d837fdcee54b
pgourish87 <pgourish012@gmail.com>
2024-02-02 19:20:07 +0530

    Created Angular project with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteFormComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
